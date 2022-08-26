67fe0c9b5639fdd551da079a0198ef2380be8811
Apfe1Saft <foraaivanov@gmail.com>
2022-08-26 13:24:05 +0300

    all tests complete

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -3,7 +3,7 @@ package ru.practicum.shareit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-//
+
 @SpringBootApplication
 public class ShareItApp {
     static ConfigurableApplicationContext context;
